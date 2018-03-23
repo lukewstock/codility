76560aee4726bf86a05ef4b1e0f2f9d0472971f5
lukewstock <lukewstock@gmail.com>
2018-03-23 22:28:33 +0000

    remove pointless parameter

--- LessonUnitTests/CyclicRotatorShould.cs
+++ LessonUnitTests/CyclicRotatorShould.cs
@@ -1,6 +1,5 @@
 ﻿namespace LessonUnitTests
 {
-    using System;
     using System.Linq;
     using FluentAssertions;
     using NUnit.Framework;
@@ -8,16 +7,16 @@
     [TestFixture]
     public class CyclicRotatorShould
     {
-        [TestCase(new[] { 1, 1, 1 }, 3, 3, new [] { 1, 1, 1 })]
-        [TestCase(new[] { 1, 2, 3 }, 3, 15, new [] { 1, 2, 3 })]
-        [TestCase(new[] { 1, 2, 2 }, 3, 1, new [] { 2, 1, 2 })]
-        [TestCase(new[] { 1, 3, 2, 2 }, 4, 2, new [] { 2, 2, 1, 3 })]
-        [TestCase(new[] { 1, 3 }, 2, 2, new [] { 1, 3 })]
-        [TestCase(new[] { 1, 3 }, 2, 3, new [] { 3, 1 })]
-        [TestCase(new[] { 1, 3 }, 2, 15, new [] { 3, 1 })]
-        public void DoNothing_WhenTheLengthOfArrayMatchesNumberToShiftBy(int[] arrayToShift, int arrayLength, int shiftRightBy, int[] expectedArray)
+        [TestCase(new[] { 1, 1, 1 }, 3, new [] { 1, 1, 1 })]
+        [TestCase(new[] { 1, 2, 3 }, 15, new [] { 1, 2, 3 })]
+        [TestCase(new[] { 1, 2, 2 }, 1, new [] { 2, 1, 2 })]
+        [TestCase(new[] { 1, 3, 2, 2 }, 2, new [] { 2, 2, 1, 3 })]
+        [TestCase(new[] { 1, 3 }, 2, new [] { 1, 3 })]
+        [TestCase(new[] { 1, 3 }, 3, new [] { 3, 1 })]
+        [TestCase(new[] { 1, 3 }, 15, new [] { 3, 1 })]
+        public void DoNothing_WhenTheLengthOfArrayMatchesNumberToShiftBy(int[] arrayToShift, int shiftRightBy, int[] expectedArray)
         {
-            var result = CyclicRotator.ShiftArrayRight(arrayToShift, arrayLength, shiftRightBy);
+            var result = CyclicRotator.ShiftArrayRight(arrayToShift, shiftRightBy);
 
             result.Should().ContainInOrder(expectedArray);
         }
@@ -25,11 +24,11 @@
 
     public class CyclicRotator
     {
-        public static int[] ShiftArrayRight(int[] arrayToShift, int arrayLength, int shiftRightBy)
+        public static int[] ShiftArrayRight(int[] arrayToShift, int shiftRightBy)
         {
-            if (shiftRightBy > arrayLength)
+            if (shiftRightBy > arrayToShift.Length)
             {
-                shiftRightBy = shiftRightBy % arrayLength;
+                shiftRightBy = shiftRightBy % arrayToShift.Length;
             }
 
             var end = arrayToShift.TakeLast(shiftRightBy);
