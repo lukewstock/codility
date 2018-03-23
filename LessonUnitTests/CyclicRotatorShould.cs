e9c55f6c7c0d8c432e702a73b04c7ba52db7eed4
lukewstock <lukewstock@gmail.com>
2018-03-23 22:17:55 +0000

    working cyclic rotator solution

--- LessonUnitTests/CyclicRotatorShould.cs
+++ LessonUnitTests/CyclicRotatorShould.cs
@@ -1,21 +1,25 @@
-﻿using FluentAssertions;
-
-namespace LessonUnitTests
+﻿namespace LessonUnitTests
 {
     using System;
-    using System.Collections.Generic;
-    using System.Text;
+    using System.Linq;
+    using FluentAssertions;
     using NUnit.Framework;
 
     [TestFixture]
     public class CyclicRotatorShould
     {
-        [TestCase(new[] { 1, 1, 1 }, 3, 3)]
-        public void DoNothing_WhenTheLengthOfArrayMatchesNumberToShiftBy(int[] arrayToShift, int arrayLength, int shiftRightBy)
+        [TestCase(new[] { 1, 1, 1 }, 3, 3, new [] { 1, 1, 1 })]
+        [TestCase(new[] { 1, 2, 3 }, 3, 15, new [] { 1, 2, 3 })]
+        [TestCase(new[] { 1, 2, 2 }, 3, 1, new [] { 2, 1, 2 })]
+        [TestCase(new[] { 1, 3, 2, 2 }, 4, 2, new [] { 2, 2, 1, 3 })]
+        [TestCase(new[] { 1, 3 }, 2, 2, new [] { 1, 3 })]
+        [TestCase(new[] { 1, 3 }, 2, 3, new [] { 3, 1 })]
+        [TestCase(new[] { 1, 3 }, 2, 15, new [] { 3, 1 })]
+        public void DoNothing_WhenTheLengthOfArrayMatchesNumberToShiftBy(int[] arrayToShift, int arrayLength, int shiftRightBy, int[] expectedArray)
         {
             var result = CyclicRotator.ShiftArrayRight(arrayToShift, arrayLength, shiftRightBy);
 
-            result.Should().BeSameAs(arrayToShift);
+            result.Should().ContainInOrder(expectedArray);
         }
     }
 
@@ -23,7 +27,14 @@ namespace LessonUnitTests
     {
         public static int[] ShiftArrayRight(int[] arrayToShift, int arrayLength, int shiftRightBy)
         {
-            return arrayToShift;
+            while (shiftRightBy > arrayLength)
+            {
+                shiftRightBy = arrayLength;
+            }
+
+            var end = arrayToShift.TakeLast(shiftRightBy);
+            var begining = arrayToShift.Take(arrayToShift.Length - shiftRightBy);
+            return end.Concat(begining).ToArray();
         }
     }
 }
