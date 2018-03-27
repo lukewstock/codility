2ccbc651b569d83304923a37ea5193b9fb2ec10c
lukewstock <lukewstock@gmail.com>
2018-03-27 08:26:02 +0100

    add OddOccurances project

---
+++ LessonSolutions/OddOccurenceFinder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LessonSolutions
+{
+    public class OddOccurenceFinder
+    {
+        public static int FindMismatchingElement(int[] elements)
+        {
+            return elements[0];
+        }
+    }
+}
---
+++ LessonUnitTests/OddOccurenceFinderShould.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LessonUnitTests
+{
+    using FluentAssertions;
+    using LessonSolutions;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class OddOccurenceFinderShould
+    {
+        [Test]
+        public void ReturnInput_WhenOnlyOneElement()
+        {
+            var elements = new int [1];
+
+            var result = OddOccurenceFinder.FindMismatchingElement(elements);
+
+            result.Should().Be(elements[0]);
+        }
+    }
+}
